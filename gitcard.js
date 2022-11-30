de7f0a8ee0603b84eff5384278a5ee7432c38552
Isaac Freires <isaac.freires@osf.digital>
2022-11-30 15:31:33 -0300

    Atualização concluída

--- gitcard.js
+++ gitcard.js
@@ -1,11 +1,26 @@
-const getGitHubInfo = function () {
+//Id 
+let text = document.querySelector("#usuario-github")
+let botao = document.querySelector("#buscar-github");
+let repo = document.querySelector("#repositorios");
+//let repo_url = repo.querySelector("a");
+let seguidores = document.querySelector("#seguidores");
+
+let avatar = document.querySelector(".avatar");
+let avatarImagem = avatar.querySelector("img");
+let nome = document.querySelector(".content h1");
+
+const getGitHubInfo = function (username) {
     var url = 'https://api.github.com/users/' + username;
 
     var ajax = new XMLHttpRequest();
 
     ajax.onreadystatechange = function () {
-        if(this.readyState == 4 && this.status == 200){
-            let ajax = JSON.parse(this.responseText);
+        if (this.readyState == 4 && this.status == 200) {
+            let xhr = JSON.parse(this.responseText);
+            avatarImagem.src = xhr.avatar_url;
+            nome.innerHTML = xhr.name;
+            seguidores.href = xhr.followers_url;
+            repo.href = xhr.repos_url;
         }
     };
 
@@ -13,3 +28,8 @@ const getGitHubInfo = function () {
     ajax.send();
 
 };
+
+botao.addEventListener("click", function (e) {
+    e.preventDefault();
+    getGitHubInfo(text.value);
+});
